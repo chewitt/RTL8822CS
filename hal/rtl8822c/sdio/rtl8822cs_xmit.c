35d02ae882729735c4bac92b2f58bbdbfa414b01
chewitt <christianshewitt@gmail.com>
2020-10-30 12:49:25 +0000

    fix build issues on Linux 5.9

--- hal/rtl8822c/sdio/rtl8822cs_xmit.c
+++ hal/rtl8822c/sdio/rtl8822cs_xmit.c
@@ -453,9 +453,7 @@ thread_return rtl8822cs_xmit_thread(thread_context context)
 	u8 thread_name[20] = "RTWHALXT";
 #ifdef RTW_XMIT_THREAD_HIGH_PRIORITY_AGG
 #ifdef PLATFORM_LINUX
-	struct sched_param param = { .sched_priority = 1 };
-
-	sched_setscheduler(current, SCHED_FIFO, &param);
+	sched_set_fifo_low(current);
 #endif /* PLATFORM_LINUX */
 #endif /* RTW_XMIT_THREAD_HIGH_PRIORITY_AGG */
 
